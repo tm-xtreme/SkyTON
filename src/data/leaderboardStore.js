a6e8bdbc19e93dbfed36af179fb411d974856af9
tm-xtreme <teamxtrm.bd@gmail.com>
2025-05-06 12:24:12 +0600

    Update leaderboardStore.js

--- src/data/leaderboardStore.js
+++ src/data/leaderboardStore.js
@@ -1,9 +1,6 @@
-
-import React from 'react';
-import { getLeaderboard } from '@/data/firestore/leaderboardActions'; // Updated import path
+import { getLeaderboard } from '@/data/firestore/leaderboardActions';
 
 // Fetches the leaderboard data from Firestore
 export const getLeaderboardData = async (count = 10) => {
   return await getLeaderboard(count);
 };
-  
