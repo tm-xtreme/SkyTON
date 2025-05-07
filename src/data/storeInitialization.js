497c51c87d5f68b231cb374690470bdda4e7358f
tm-xtreme <teamxtrm.bd@gmail.com>
2025-05-07 21:36:25 +0600

    Update storeInitialization.js

--- src/data/storeInitialization.js
+++ src/data/storeInitialization.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import { getOrCreateUser } from '@/data/firestore/userActions'; // Updated import path
-import { seedInitialTasks } from '@/data/firestore/initActions'; // Updated import path
+// /data/storeInitialization.js
+import { getOrCreateUser } from '@/data/firestore/userActions';
+import { seedInitialTasks } from '@/data/firestore/initActions';
 import { parseLaunchParams } from '@/data/telegramUtils';
 import { defaultFirestoreTasks } from '@/data/defaults';
 
 export const initializeAppData = async () => {
-  console.log("Initializing App Data...");
-  const { telegramUser, referrerId } = parseLaunchParams();
+  console.log('Initializing App Data...');
+  const { telegramUser, referrerId, isTelegram } = parseLaunchParams();
 
   await seedInitialTasks(defaultFirestoreTasks);
 
   // Allow fallback for dev/test mode
+  if (!isTelegram) {
+    console.warn('Non-Telegram user trying to access the app.');
+    return { error: 'non-telegram' };
+  }
+
   if (!telegramUser) {
-    console.warn("No Telegram user data found in URL. Using dummy user for dev/test.");
-    return null;/*{
-      id: 'test_user',
-      name: 'Test User',
-      isAdmin: true, // or false if you want to test as a regular user
-    };*/
+    console.warn('No Telegram user data found in URL.');
+    return null;
   }
 
   const userData = await getOrCreateUser(telegramUser, referrerId);
 
   if (!userData) {
-    console.error("Failed to get or create user in Firestore.");
+    console.error('Failed to get or create user in Firestore.');
     return null;
   }
 
-  console.log("App data initialized. User:", userData.id);
+  console.log('App data initialized. User:', userData.id);
   return userData;
 };
