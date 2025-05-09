4bd335ad7ced909acc7642c0930228ce23d6aa80
tm-xtreme <teamxtrm.bd@gmail.com>
2025-05-09 12:53:40 +0600

    Update serverFirebase.js

--- src/lib/serverFirebase.js
+++ src/lib/serverFirebase.js
@@ -1,5 +1,9 @@
 import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
+import dotenv from 'dotenv';
+
+// Load the environment variables
+dotenv.config();
 
 if (!getApps().length) {
   try {
@@ -7,9 +11,10 @@ if (!getApps().length) {
       credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Ensure newlines are handled
       }),
     });
+    console.log('Firebase app initialized');
   } catch (e) {
     console.error('Firebase admin initialization error', e);
   }
