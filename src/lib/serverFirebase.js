90f766d840a22ada8a3266e98a5f613e26db2353
tm-xtreme <teamxtrm.bd@gmail.com>
2025-05-09 12:34:58 +0600

    Update serverFirebase.js

--- src/lib/serverFirebase.js
+++ src/lib/serverFirebase.js
@@ -1,18 +1,17 @@
-import { initializeApp, cert } from 'firebase-admin/app';
+import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-let app;
-try {
-  app = initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    }),
-  });
-} catch (e) {
-  if (!/already exists/u.test(e.message)) {
-    console.error('Firebase admin initialization error', e.stack);
+if (!getApps().length) {
+  try {
+    initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      }),
+    });
+  } catch (e) {
+    console.error('Firebase admin initialization error', e);
   }
 }
 
