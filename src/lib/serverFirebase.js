bf6f5ee2d59d1f68de579ffbfb59b78355667026
tm-xtreme <teamxtrm.bd@gmail.com>
2025-05-09 14:20:58 +0600

    Update serverFirebase.js

--- src/lib/serverFirebase.js
+++ src/lib/serverFirebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from 'firebase-admin/firestore';
 const serviceAccount = {
   type: 'service_account',
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
