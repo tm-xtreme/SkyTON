1fe27ff05ef129951ef6ae97d2477af1e0c105c8
tm-xtreme <teamxtrm.bd@gmail.com>
2025-05-10 10:40:08 +0600

    Update serverFirebase.js

--- src/lib/serverFirebase.js
+++ src/lib/serverFirebase.js
@@ -1,20 +1,16 @@
 import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-// Construct the service account object
 const serviceAccount = {
-  type: 'service_account',
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\n/g, '\n'),
 };
 
-// Initialize the Firebase Admin app if not already initialized
 if (!getApps().length) {
   initializeApp({
     credential: cert(serviceAccount),
   });
 }
 
-// Export the Firestore database
 export const db = getFirestore();
