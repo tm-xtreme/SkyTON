f89becc01746a09cf6f061b2fd7feabfc7a472d1
tm-xtreme <teamxtrm.bd@gmail.com>
2025-05-25 01:31:56 +0600

    Update adminActions.js

--- src/data/firestore/adminActions.js
+++ src/data/firestore/adminActions.js
@@ -60,11 +60,15 @@ export const getPendingVerifications = async () => {
     for (const user of allUsers) {
       if (Array.isArray(user.pendingVerificationTasks) && user.pendingVerificationTasks.length > 0) {
         for (const taskId of user.pendingVerificationTasks) {
+          // --- NEW: Get details from pendingVerificationDetails if present ---
+          const details = user.pendingVerificationDetails?.[taskId] || {};
           pendingItems.push({
             userId: user.id,
             username: user.username || user.firstName || `User ${user.id}`,
-            taskId
-            // Task details will be filled in AdminPage
+            taskId,
+            title: details.title || undefined,
+            target: details.target || undefined,
+            reward: details.reward || undefined
           });
         }
       }
